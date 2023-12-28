ab17fe3b5af937f671f74443c508876d23490169
Masaru2 <dmasaru3@gmail.com>
2023-12-27 21:29:19 -0300

    Fix wrong encounter rate in route 22

--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -24,14 +24,14 @@ IF DEF(_GREEN)
 	db  4, NIDORAN_M
 ENDC
 IF DEF(_BLUE)
-	db  3, NIDORAN_M
+	db  3, NIDORAN_F
 	db  4, RATTATA
-	db  4, NIDORAN_M
+	db  4, NIDORAN_F
 	db  2, RATTATA
-	db  2, NIDORAN_M
+	db  2, NIDORAN_F
 	db  3, SPEAROW
 	db  5, SPEAROW
-	db  3, NIDORAN_F
-	db  4, NIDORAN_F
+	db  3, NIDORAN_M
+	db  4, NIDORAN_M
 ENDC
 	db 0 ; water encounter rate
