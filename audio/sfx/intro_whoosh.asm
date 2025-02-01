4c72fadca29923b43b0958a7ed59bad59103a302
Masaru2 <dmasaru3@gmail.com>
2025-01-31 23:08:33 -0300

    R/G scrolling SFX fully restored

--- audio/sfx/intro_whoosh.asm
+++ audio/sfx/intro_whoosh.asm
@@ -6,7 +6,7 @@ IF DEF(_BLUE)
 	noise_note 3, 12, 0, 34
 	noise_note 15, 13, 2, 36
 ELSE
-	noise_note 8, 3, 0, 32
+	noise_note 8, 3, -4, 32
 	noise_note 6, 10, 0, 32
 	noise_note 8, 11, 0, 33
 	noise_note 10, 12, 0, 34
